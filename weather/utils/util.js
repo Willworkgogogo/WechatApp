dc547781668782a71f2cfc4062244ce786665527
wangweiwei <will_workhard@163.com>
2017-03-12 22:54:01 +0800

    complete units

--- weather/utils/util.js
+++ weather/utils/util.js
@@ -1,21 +1,135 @@
-function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
-
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+// getLocation 获取当前位置坐标
+function getLocation(callback) {
+  wx.getLocation({
+    type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+    success: function(res){
+      callback(true, res.latitude, res.longitude)
+    },
+    fail: function() {
+      callback(false)
+    }
+    
+  })
 }
 
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+
+// getWeatherByLocation 获取指定位置的天气信息
+function getWeatherByLocation(latitude, longitude, callback) {
+  var apiKey = "你自己的Key",
+      apiURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude + "?lang=zh&units=ca"
+
+  wx.request({
+    url: apiURL,
+    method: 'GET',
+    // header: {}, // 设置请求的 header
+    success: function(res){
+      var weatherData = parseWeatherData(res.data);
+      getCityName(latitude, longitude, function(city){
+        weatherData.city = city;
+        callback(weatherData)
+      })
+    },
+    fail: function() {
+      // fail
+    },
+    complete: function() {
+      // complete
+    }
+  })
+}
+
+// parseWeatherData 解析天气数据
+function parseWeatherData(data) {
+
+  var weather = {};
+  weather["current"] = data.currently;
+  weather["daily"] = data.daily;
+
+  return weather;
+}
+
+// getCityName 根据经纬度获取城市名称
+function getCityName(latitude, longitude, callback) {
+  var apiURL = "http://api.map.baidu.com/geocoder?output=json&location=" + latitude + "," + longitude + "&key=37492c0ee6f924cb5e934fa08c6b1676";
+
+  wx.request({
+    url: apiURL,
+    method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    // header: {}, // 设置请求的 header
+    success: function(res){
+      callback(res.data["result"]["addressComponent"]["city"]);
+    }
+  })
+}
+
+// formatDate格式化时间戳为日期
+function formatDate(timestamp) {
+
+  var date = new Date(timestamp * 1000);
+  
+  return date.getMonth() + 1 + "月" + date.getDate() + "日" + formatWeekday(timestamp);
+}
+
+// formatTime 将时间戳格式化为时间
+function formatTime(timestamp) {
+
+  var date = new Date(timestamp * 1000),
+      weekday = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+      index = date.getDay();
+
+  return weekday[index];
+}
+
+// requestWeatherData加载天气数据
+function requestWeatherData(cb) {
+
+  getLocation(function(success, latitude, longitude){
+    
+    // 如果GPS信息获取不成功， 设置一个默认坐标
+    if (success == false) {
+
+      latitude = 39.90403;
+      longitude = 116.407526;
+    }
+
+    // 请求天气数据 API
+    getWeatherByLocation(latitude, longitude, function(){
+
+      cb(weatherData);
+
+    })
+  })
+}
+
+// loadWeatherData
+function loadWeatherData(callback) {
+
+  requestWeatherData(function(data){
+
+    // 对原始数据做一些修整， 然后输出给前端
+    var weatherData = {};
+    weatherDataCurrent = data.current;
+    weatherDataCurrent.formattedData = formatDate(data.current.time);
+    weatherDataCurrent.formattedTime = formatTime(data.current.time);
+    weatherDataCurrent.temperature = parseInt(weatherDataCurrent.temperature);
+
+    var wantedDaily = [];
+    for(var i = 1; i < weatherData.daily.data.length; i++) {
+
+      var wantedDailyItem = weatherData.daily.data[i],
+          time = weatherData.daily.data[i].time;
+          wantedDailyItem["weekday"] = formatWeekday(time);
+          wantedDailyItem["temperatureMin"] = parseInt(weatherData.daily.data[i]["temperatureMin"])
+          wantedDailyItem["temperatureMax"] = parseInt(weatherData.daily.data[i]["temperatureMax"])
+      
+      wantedDaily.push(wantedDailyItem);
+    }
+
+    weatherData.daily.data = wantedDaily;
+    callback(weatherData);
+  })
 }
 
 module.exports = {
-  formatTime: formatTime
+  loadWeatherData: loadWeatherData
 }
